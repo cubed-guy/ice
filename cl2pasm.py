47139002bf399bf9948b90aee553201900dd8e0b
cubed-guy <47624167+cubedGuy@users.noreply.github.com>
2021-01-30 17:38:40 +0530

    Fine tuning
    - Better regex patterns for better testing.
    - Better comment management for better testing.
    - Better declaration messages for better testing.
    
    - Declexp of the first method argument is now optional.
    - Multiple declarations with same declexp now allowed.
    - Disallowed more than one colon (':') character per line.

--- cl2pasm.py
+++ cl2pasm.py
@@ -10,18 +10,18 @@ Pass 2 - method substitution (including __data_definition__ methods)
 '''
 
 from sys import argv
-if   len(argv) <2: raise ValueError('Input file not specified')
+# if   len(argv) <2: raise ValueError('Input file not specified')
+if   len(argv) <2: argv.append('Test file.cl')
 # if   len(argv) <2: print('Input file not specified'); quit(1)
 elif len(argv)==2: argv.append('a.pasm')
 infile  = open(argv[1])
-outfile = open(argv[2], 'w')
 # argv[0] is the path of this python file
 
 class dPrinter: __rmatmul__ = lambda self, other: (print('', other), other)[1]
 d = dPrinter()
 
 def err(Error, message = None):
-	print(f'"{argv[1]}", error in line {line_no}')
+	print(f'File "{argv[1]}", line {line_no}')
 	print('\t'+line.strip())
 	if message is None: print(Error)
 	else: print(Error, message, sep = ': ')
@@ -30,44 +30,52 @@ def err(Error, message = None):
 ''' ------------------------------PATTERNS-------------------------------- '''
 import re
 
-space_pattern = re.compile(r'\s+')
-word_re = r'[\w_][\w\d_]*'
+word_re = r'[a-zA-Z_][a-zA-Z\d_]*'
 word_pattern = re.compile(word_re)
 space_pattern = re.compile(r'\s*')
 exp_re = fr'(\d*|{word_re})'	# only identifiers and numbers for now
 
 unit_re = r'[0-8]'	# will add tuples later
-shape_re = r'\*?(\[\d+\]|[0-8])*'
+shape_re = r'\*?(\[\d+\]|[0-8])*?'
 
-declexp_re = fr'({shape_re}{unit_re})({word_re})'
-func_declexp_re = fr'({shape_re}{unit_re})?({word_re})'
+decl_re = fr'({shape_re}{unit_re})({word_re})'
+optional_decl_re = fr'({shape_re}{unit_re})?({word_re})'
 args_re = fr'\(({exp_re}(\s*,\s*{exp_re})*)?\)'
-dec_pattern = re.compile(declexp_re)
-func_pattern = re.compile(func_declexp_re+args_re)
-label_pattern = re.compile('#'+declexp_re+fr'(\({word_re}\))?'+':')
-
-blank_pattern = re.compile(r'\s*(--.*)?$')
+head_args_re = (fr'\(((?P<first>{optional_decl_re})(\s*,\s*{decl_re})*)?\)')
+head_args_pattern = re.compile(head_args_re)
+decl_pattern = re.compile(decl_re)
+func_pattern = re.compile(optional_decl_re+fr'(?P<args>{head_args_re})')
+label_pattern = re.compile('#'+decl_re+fr'(?P<parent>\({word_re}\))?'+':')
 
 alloc_re = fr'\[{exp_re}\]+'	# shape and unit alloc later
 alloc_pattern = re.compile(alloc_re+word_re)
 
 '''  -------------------------PASS 1 - CREATING DICTS--------------------- '''
 def getVars(region, level_ = None):
+	Dict = data[head_label][1][head_func][1]
+	
 	if level_ is None: level_ = level
-	decls = dec_pattern.findall(region)
+	decls = decl_pattern.finditer(region)
+	args = head_args_pattern.match(region)
+	if args:
+		first_arg = args["first"]
+		if head_label:
+			Dict[first_arg] = data[head_label][0]
+			print(line_no, ' '*level_+#f'under {head_label}.{head_func} '
+			f'{first_arg} of shape {data[head_label][0]}.', sep = '\t')
+		
 	for decl in decls:
-		declexp = decl[0]
-		var = decl[2]
-		Dict = data[head_label][1][head_func][1]
-		if var in Dict: err('ValueError', f"Label '{var}' already declared.")
-		print(line_no, ' '*level_+f'under {head_label}.{head_func} {var = } of {declexp}')
-		Dict[var] = declexp
+		declexp = decl[1]
+		var = decl[3]
+		print(line_no, ' '*level_+#f'under {head_label}.{head_func} 
+			f'{var} of shape {declexp}.', sep = '\t')
+		if var not in Dict: Dict[var] = declexp; continue
+		if Dict[var] != declexp: err('ValueError', "Declaring variable "
+			f"'{var}' with declexp {declexp}. "
+			f"(Already declared with {Dict[var]})")
 
 data = {'': (None, {'':(None, {})}, None)}
 # {label: (label_type, {func: (func_type, {var: var_type})}, parent)}
-
-indent = ''
-meth_indent = ''	# indentation level of method header
 
 loc = 0
 head_func = ''
@@ -76,8 +84,12 @@ indent_stack = []
 expect_indent = False
 
 for line_no, line in enumerate(infile, 1):
-	# ignore blank/commented lines
-	if blank_pattern.match(line): continue
+	# ignore comment
+	# TODO: don't ignore '--' if in string
+	line = line[:-1].partition('--')[0]
+
+	# ignore blank lines
+	if not line or line.isspace(): continue
 
 	# INDENTATION
 	curr_indent = space_pattern.match(line)[0]
@@ -88,7 +100,7 @@ for line_no, line in enumerate(infile, 1):
 	      indent_diff = indent_diff[len(indent):]
 	    elif indent_diff:err('TabError','inconsistent use of tabs and spaces.')
 	    else: break	# dedent
-	  # indent if indent_diff else dedent (if exhausted)
+	  # indent if indent_diff else dedent
 	  if indent_diff: indent_stack.append(indent_diff); level_diff = 1
 	  else: level_diff = level-len(indent_stack); del indent_stack[level:]
 	else: level_diff = 0
@@ -101,7 +113,8 @@ for line_no, line in enumerate(infile, 1):
 
 	line = line.strip()
 	parts = line.partition(':')
-	expect_indent = parts[1] and bool(blank_pattern.match(parts[2]))
+	expect_indent = parts[1] and bool(space_pattern.fullmatch(parts[2]))
+	# if line and not line.isspace(): print(' '*level+line[:-1])
 
 	if level_diff < 0:	# if dedent
 		if not level: head_func = head_label = ''
@@ -111,12 +124,15 @@ for line_no, line in enumerate(infile, 1):
 	if   decl:=label_pattern.match(line):
 		declexp = decl[1]
 		label = decl[3]
-		parent = decl[4]
 		Dict = data
+		print(line_no, ' '*level+#f'under {head_label}.{head_func} '
+			f'#{label} of shape {declexp}', sep = '\t', end = ' ')
 		if label in Dict:err('ValueError',f"Label '{label}' already declared.")
-		print(line_no, ' '*level+f'under {head_label}.{head_func} {label = } of {declexp}')
 		
-		if not parent: parent = None
+		parent = decl['parent']
+		if parent: parent = parent[1:-1]
+		else: parent = None
+		print(f"extends {parent!r}.")
 		Dict[label] = (declexp, {}, parent)
 		head_label = label
 
@@ -124,15 +140,19 @@ for line_no, line in enumerate(infile, 1):
 		declexp = decl[1]
 		func = decl[3]
 		Dict = data[head_label][1]
-		if func in Dict: err('ValueError',f"Label '{func}' already declared.")
-		print(line_no, ' '*level+f'under {head_label}.{head_func} {func = } of {declexp}')
+		print(line_no, ' '*level+#f'under {head_label}.{head_func} '
+			f'{func}() of shape {declexp}', sep = '\t')
+		if func in Dict: err('ValueError', f"Function '{func}' already declared.")
 		
 		Dict[func] = (declexp, {})
 		head_func = func
-		args = decl[4]
+		args = decl['args']
 		getVars(args, level+1)
+		getVars(line.partition(':')[2])
 
-	else: getVars(line.partition('--')[0])
+	elif line.count(':') > 1: err('SyntaxError', 'invalid syntax.')
+
+	else: getVars(line)
 
 	loc += len(line)
 
