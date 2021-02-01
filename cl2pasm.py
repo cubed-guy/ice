7173a4463fc906d312208a4126c09e7aefdae94b
cubed-guy <47624167+cubedGuy@users.noreply.github.com>
2021-02-01 16:01:51 +0530

    Commencement of Pass 2

--- cl2pasm.py
+++ cl2pasm.py
@@ -10,13 +10,17 @@ Pass 2 - method substitution (including __data_definition__ methods)
 '''
 
 from sys import argv
-if   len(argv) <2: argv.append('Test file.cl')
-# if   len(argv) <2: print('Input file not specified'); quit(1)
-elif len(argv)==2: argv.append('a.pasm')
+# if len(argv) <2: print('Input file not specified'); quit(1)
+if len(argv)<2: argv.append('Test file.cl')
+if len(argv)<3: argv.append('a.pasm')
 infile  = open(argv[1])
 # argv[0] is the path of this python file
 
-class dPrinter: __rmatmul__ = lambda self, other: (print('', other), other)[1]
+ismodule = __name__ != '__main__'
+dprint = (lambda *args, **kwargs: None) if ismodule else print
+class dPrinter:
+	if ismodule: __rmatmul__ = lambda self, other: other
+	else: __rmatmul__ = lambda self, other: (print('', other), other)[1]
 d = dPrinter()
 
 def err(Error, message = None):
@@ -29,52 +33,57 @@ def err(Error, message = None):
 ''' ------------------------------PATTERNS-------------------------------- '''
 import re
 
-word_re = r'[a-zA-Z_][a-zA-Z\d_]*'
-word_pattern = re.compile(word_re)
-space_pattern = re.compile(r'\s*')
-exp_re = fr'(\d*|{word_re})'	# only identifiers and numbers for now
+class Patterns:
+	word_re = r'[a-zA-Z_][a-zA-Z\d_]*'
+	word = re.compile(word_re)
+	space = re.compile(r'\s*')
+	exp_re = fr'(\d*|{word_re})'	# only identifiers and numbers for now
 
-unit_re = r'[0-8]'	# will add tuples later
-shape_re = r'\*?(\[\d+\]|[0-8])*?'
+	unit_re = r'[0-8]'	# will add tuples later
+	shape_re = r'\*?(\[\d+\]|[0-8])*?'
 
-decl_re = fr'({shape_re}{unit_re})({word_re})'
-optional_decl_re = fr'({shape_re}{unit_re})?({word_re})'
-args_re = fr'\(({exp_re}(\s*,\s*{exp_re})*)?\)'
-head_args_re = (fr'\(((?P<first>{optional_decl_re})(\s*,\s*{decl_re})*)?\)')
-head_args_pattern = re.compile(head_args_re)
-decl_pattern = re.compile(decl_re)
-func_pattern = re.compile(optional_decl_re+fr'(?P<args>{head_args_re})')
-label_pattern = re.compile('#'+decl_re+fr'(?P<parent>\({word_re}\))?'+':')
+	decl_re = fr'({shape_re}{unit_re})({word_re})'
+	ident_re = fr'({shape_re}{unit_re})?({word_re})'
+	args_re = fr'\(({exp_re}(\s*,\s*{exp_re})*)?\)'
+	head_args_re = (fr'\(((?P<first>{ident_re})(\s*,\s*{ident_re})*)?\)')
+	head_args = re.compile(head_args_re)
+	decl  = re.compile(decl_re)
+	ident = re.compile(ident_re)
+	func  = re.compile(ident_re+fr'(?P<args>{head_args_re})')
+	label = re.compile('#'+decl_re+fr'(?P<parent>\({word_re}\))?'+':')
 
-alloc_re = fr'\[{exp_re}\]+'	# shape and unit alloc later
-alloc_pattern = re.compile(alloc_re+word_re)
+	alloc_re = fr'\[{exp_re}\]+'	# shape and unit alloc later
+	alloc = re.compile(alloc_re+word_re)
 
 '''  -------------------------PASS 1 - CREATING DICTS--------------------- '''
 def getVars(region, level_ = None):
 	Dict = data[head_label][1][head_func][1]
 	
 	if level_ is None: level_ = level
-	decls = decl_pattern.finditer(region)
-	args = head_args_pattern.match(region)
+	decls = Patterns.decl.finditer(region)
+	args = Patterns.head_args.match(region)
 	if args:
 		first_arg = args["first"]
 		if head_label:
-			Dict[first_arg] = data[head_label][0]
-			print(line_no, ' '*level_+#f'under {head_label}.{head_func} '
+			Dict[first_arg] = (data[head_label][0], [])
+			dprint(line_no, ' '*level_+#f'under {head_label}.{head_func} '
 			f'{first_arg} of shape {data[head_label][0]}.', sep = '\t')
 		
 	for decl in decls:
 		shape = decl[1]
 		var = decl[3]
-		print(line_no, ' '*level_+#f'under {head_label}.{head_func} 
+		dprint(line_no, ' '*level_+#f'under {head_label}.{head_func} 
 			f'{var} of shape {shape}.', sep = '\t')
-		if var not in Dict: Dict[var] = shape; continue
+		if var not in Dict: Dict[var] = (shape, []); continue
 		if Dict[var] != shape: err('ValueError', "Declaring variable "
 			f"'{var}' with shape {shape}. "
-			f"(Already declared with {Dict[var]})")
+			f"(Already declared with {Dict[var][0]})")
 
 data = {'': (None, {'':(None, {})}, None)}
-# {label: (label_type, {func: (func_type, {var: var_type})}, parent)}
+# {label:
+# 	(label_type,
+# 	{func: (func_type, {var: (var_type, label_stack)})},
+# 	parent)}
 
 loc = 0
 head_func = ''
@@ -91,7 +100,7 @@ for line_no, line in enumerate(infile, 1):
 	if not line or line.isspace(): continue
 
 	# INDENTATION
-	curr_indent = space_pattern.match(line)[0]
+	curr_indent = Patterns.space.match(line)[0]
 	if curr_indent != ''.join(indent_stack):
 	  indent_diff = curr_indent
 	  for level, indent in enumerate(indent_stack):
@@ -112,34 +121,33 @@ for line_no, line in enumerate(infile, 1):
 
 	line = line.strip()
 	parts = line.partition(':')
-	expect_indent = parts[1] and bool(space_pattern.fullmatch(parts[2]))
-	# if line and not line.isspace(): print(' '*level+line[:-1])
+	expect_indent = parts[1] and (not parts[2] or parts[2].isspace())
 
 	if level_diff < 0:	# if dedent
 		if not level: head_func = head_label = ''
 		elif head_label and level == 1: head_func = ''
 
 	# UPDATE DICT
-	if   decl:=label_pattern.match(line):
+	if   decl:=Patterns.label.match(line):
 		shape = decl[1]
 		label = decl[3]
 		Dict = data
-		print(line_no, ' '*level+#f'under {head_label}.{head_func} '
+		dprint(line_no, ' '*level+#f'under {head_label}.{head_func} '
 			f'#{label} of shape {shape}', sep = '\t', end = ' ')
 		if label in Dict:err('ValueError',f"Label '{label}' already declared.")
 		
 		parent = decl['parent']
 		if parent: parent = parent[1:-1]
 		else: parent = None
-		print(f"extends {parent!r}.")
+		dprint(f"extends {parent!r}.")
 		Dict[label] = (shape, {}, parent)
 		head_label = label
 
-	elif decl:=func_pattern.match(line):
+	elif decl:=Patterns.func.match(line):
 		shape = decl[1]
 		func = decl[3]
 		Dict = data[head_label][1]
-		print(line_no, ' '*level+#f'under {head_label}.{head_func} '
+		dprint(line_no, ' '*level+#f'under {head_label}.{head_func} '
 			f'{func}() of shape {shape}', sep = '\t')
 		if func in Dict: err('ValueError', f"Function '{func}' already declared.")
 		
@@ -155,4 +163,6 @@ for line_no, line in enumerate(infile, 1):
 
 	loc += len(line)
 
-for l, f in data.items(): print(repr(l), f, sep = ':\t')
+if ismodule: print('Pass 1 successful'); infile.seek(0)
+else:
+	for l, f in data.items(): print(repr(l), f, sep = ':\t')
